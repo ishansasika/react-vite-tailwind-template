75608e085dc9e24b2fda25a6995af5514d3d5073
ishansasika <ishansasika@gmail.com>
2025-12-13 11:04:12 +0530

    feat: Add action deploys

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: Update this to match your GitHub repository name
+  base: '/react-vite-tailwind-template/',
 })
